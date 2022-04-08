c1a5de1e7bb22dc855c19db6234bedcf5398e9c5
Saidnazar-EPAM <102347678+Saidnazar-EPAM@users.noreply.github.com>
2022-04-09 03:05:51 +0500

    Task completed.

--- Task2/NumberParser.cs
+++ Task2/NumberParser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Task2
 {
@@ -6,7 +7,40 @@ namespace Task2
     {
         public int Parse(string stringValue)
         {
-            throw new NotImplementedException();
+            if(stringValue == null)
+            {
+                throw new ArgumentNullException(nameof(stringValue));
+            }
+            stringValue = stringValue.Trim();
+            var match = new Regex(@"^[\+-]?\d+$");
+            if (!match.IsMatch(stringValue))
+            {
+                throw new FormatException();
+            }
+            int result = 0;
+            char sign = '0';
+            if(!Char.IsDigit(stringValue[0]))
+            {
+                sign = stringValue[0];
+                stringValue = stringValue.Remove(0, 1);
+            }
+            if(sign == '+' || sign == '0')
+            {
+                foreach (char c in stringValue)
+                {
+                    result = checked(result * 10);
+                    result = checked(result + (c - '0'));
+                }
+            }
+            else
+            {
+                foreach (char c in stringValue)
+                {
+                    result = checked(result * 10);
+                    result = checked(result - (c - '0'));
+                }
+            }
+            return result;
         }
     }
 }
